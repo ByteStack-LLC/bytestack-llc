cecdd1ce590950b685341933cd9870b13243866f
fsahnouni <fsahnoun96@gmail.com>
2025-02-13 19:46:48 -0600

    added css code

--- app/components/header_components/HamburgerDropdown.tsx
+++ app/components/header_components/HamburgerDropdown.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { PopoverButton, PopoverPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import {styles} from "./HamburgerDropdown.module.css";
 
 const HamburgerDropdown = () => {
   return (
     <div>
       <div className="flex grow items-center justify-end sm:hidden">
-        <PopoverButton className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+        {/*<PopoverButton className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">*/}
+          <PopoverButton className={styles.menuButton}>
           <span className="sr-only">Open Menu</span>
           <Bars3Icon className="h-6 w-6" aria-hidden="true" />
         </PopoverButton>
