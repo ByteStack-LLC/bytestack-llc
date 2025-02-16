8ad53ca0ef80de0acf9e25bb002f423fe485a5c4
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-02-16 18:56:25 -0500

    add card component, installed heroui/card dependencies. added skeleton files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 //import Image from "next/image";
-import LandingPage from "./ui/LandingPage";
+import LandingPage from "./pages/LandingPage";
 
 export default function Home() {
   return (
--- app/pages/LandingPage.tsx
+++ app/pages/LandingPage.tsx
@@ -1,4 +1,7 @@
+"use server";
+
 import Header from "../components/Header";
+import Card from "../components/ui/Card";
 
 const LandingPage = () => {
   return (
