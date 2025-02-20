e745a372cd4a13d4661cf1beab0a5f8dce1dea6d
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-02-19 21:30:10 -0500

    installed express-rate-limit and added rate limits to database access

--- server/index.mjs
+++ server/index.mjs
@@ -3,10 +3,17 @@ import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
 import requests from "./routes/requests.mjs";
+import rateLimit from "express-rate-limit";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 
