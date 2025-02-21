71d93999ea4a0cee9fd31eb6e3ae9f16844d4efc
ortizfe <79375454+ortizfe@users.noreply.github.com>
2025-02-20 22:56:34 -0500

    updating form to version1 to go live (?)

---
+++ app/components/Team.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { motion } from "motion/react";
+import fede from "../../public/fede.png";
+import fares from "../../public/fares.png";
+import Image from "next/image";
+
+const teamData = [
+  {
+    image: fede,
+    title: "Software Engineer || Full Stack Developer",
+    description:
+      "Federico is a professional Developer with 2-3 years of of Software Engineering experience, 1-2 years of Full Stack Development and Automation Engineering experience, and 1 year of Data Engineering experience.",
+    technologies: [
+      "JavaScript",
+      "Python",
+      "Java",
+      "SQL",
+      "TypeScript",
+      "React",
+      "Next.js",
+      "Angular",
+      "Node.js",
+      "Express",
+      "Cypress",
+    ],
+  },
+
+  {
+    image: fares,
+    title: "Tech Support Lead || Full Stack Developer",
+    description:
+      "Fares is a professional Developer with 2-3 years of of Software Engineering experience, 1-2 years of Full Stack Development and Automation Engineering experience, and 1 year of Data Engineering experience.",
+    technologies: [
+      "JavaScript",
+      "Microsoft Power Platform",
+      "React",
+      "Next.js",
+      "Cypress",
+    ],
+  },
+];
+
+const ScrollReveal = ({ children }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+const TeamCard = ({ teammate }) => {
+  return (
+    <ScrollReveal>
+      <div className="flex flex-col items-center gap-8 md:flex-row">
+        <Image
+          src={teammate.image}
+          alt=""
+          className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 sm:w-[100px] md:w-[150px]"
+        />
+
+        <div className="flex flex-col gap-5">
+          <div className="flex flex-col gap-5">
+            <div className="text-xl font-semibold">{teammate.title}</div>
+            <p className="text-gray-400">{teammate.description}</p>
+          </div>
+
+          <div className="flex flex-wrap gap-5">
+            {teammate.technologies.map((tech: string, index: number) => (
+              <span key={index} className="rounded-2xl bg-black p-2">
+                {tech}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </ScrollReveal>
+  );
+};
+
+const Team = () => {
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  return (
+    <div
+      id="team"
+      className="flex min-h-[200vh] w-full flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24"
+    >
+      <ScrollReveal>
+        <h1 className="text-4xl font-light text-white md:text-6xl">
+          Meet the Team
+        </h1>
+      </ScrollReveal>
+
+      <div className="flex w-full max-w-[1000px] flex-col gap-16 text-white">
+        {teamData.map((teammate, index: number) => (
+          <TeamCard key={index} teammate={teammate} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Team;
--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 //import ThemeToggle from "../ui/theme-toggle";
 import { useState } from "react";
 import { BiX, BiMenu } from "react-icons/bi";
-import { FaInfoCircle } from "react-icons/fa";
+//import { FaInfoCircle } from "react-icons/fa";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,7 +14,7 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-500 bg-gray-800 px-16 py-6 text-white backdrop-blur-md md:justify-evenly">
+    <nav className="fixed top-0 z-10 flex w-full items-center justify-between border-b border-b-gray-500 bg-gray-800 px-16 py-6 text-white backdrop-blur-md md:justify-items-stretch">
       <Link
         href="/"
         className=" opacity-80 text-2xl bg-gradient-to-r from-[rgba(169,241,223)] to-[rgba(255,187,187)] bg-clip-text text-transparent font-semibold transition-all duration-300 hover:opacity-100 hover:text-gray-800 hover:scale-110 hover:bg-clip-text hover:text-transparent"
@@ -24,31 +24,31 @@ const NavBar = () => {
 
       <ul className="hidden md:flex gap-12">
         <Link
-          href="#home"
+          href="/#home"
           className="cursor-pointer text-white opacity-80 font-semibold transition-all duration-300 hover:opacity-100 hover:scale-110"
         >
           <li>Home</li>
         </Link>
         <Link
-          href="#tech"
+          href="/#tech"
           className="cursor-pointer text-white opacity-80 font-semibold transition-all duration-300 hover:opacity-100 hover:scale-110"
         >
           <li>Tech</li>
         </Link>
         <Link
-          href="#services"
+          href="/#team"
           className="cursor-pointer text-white opacity-80 font-semibold transition-all duration-300 hover:opacity-100 hover:scale-110"
         >
-          <li>Services</li>
+          <li>Team</li>
         </Link>
         <Link
-          href="#contact"
+          href="/#contact"
           className="cursor-pointer text-white opacity-80 font-semibold transition-all duration-300 hover:opacity-100 hover:scale-110"
         >
           <li>Contact</li>
         </Link>
       </ul>
-      {/*<ThemeToggle />*/}
+      {/*<ThemeToggle />
       <ul className="hidden md:flex gap-4 items-center">
         <Link
           href="/about"
@@ -58,7 +58,7 @@ const NavBar = () => {
             <FaInfoCircle />
           </li>
         </Link>
-      </ul>
+      </ul>*/}
 
       {isOpen ? (
         <BiX
@@ -74,7 +74,7 @@ const NavBar = () => {
 
       {isOpen && (
         <div
-          className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-l border-gray-500 bg-gray-700 p-12 ${
+          className={`fixed right-0 top-[84px] flex h-screen w-1/2 flex-col items-start justify-start gap-10 border-l border-gray-500 bg-gray-800 p-12 ${
             isOpen ? "block" : "hidden"
           }`}
         >
@@ -104,7 +104,7 @@ const NavBar = () => {
               <li>Contact</li>
             </Link>
           </ul>
-          <ul className="flex flex-wrap gap-4">
+          {/*<ul className="flex flex-wrap gap-4">
             <Link
               href="/about"
               className="cursor-pointer text-xl opacity-70 transition-all duration-300 hover:opacity-100 hover:text-blue-400 hover:scale-110"
@@ -113,7 +113,7 @@ const NavBar = () => {
                 <FaInfoCircle />
               </li>
             </Link>
-          </ul>
+          </ul>*/}
         </div>
       )}
     </nav>
